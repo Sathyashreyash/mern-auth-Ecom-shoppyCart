d51f793b2e669449918d0159cd005a2015de5896
Sathya-G Kamble <ashish.natural192@gmail.com>
2024-01-03 15:36:11 +0530

    install react js and tailwind css and create the first tamplate

---
+++ shoppyCart-ClientSide/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>App</div>
+  )
+}
+
+// git inti 
+// git add .
+// git comit -m 'install react js and tailwind css and create the first tamplate'
