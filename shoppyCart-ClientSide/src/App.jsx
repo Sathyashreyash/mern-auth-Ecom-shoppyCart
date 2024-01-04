29b2fb69f16afe83ff9e117a21b85824947530c3
Sathyashreyash <ashish.natural192@gmail.com>
2024-01-04 19:17:27 +0530

    add routes and create pages

--- shoppyCart-ClientSide/src/App.jsx
+++ shoppyCart-ClientSide/src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import { About } from "./pages/About";
+import { SingIn } from "./pages/SingIn";
+import { SingUp } from "./pages/SingUp";
+import { Profile } from "./pages/Profile";
 
-export default function App() {
+function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="home" element={<Home />}></Route>
+        <Route path="about" element={<About />}></Route>
+        <Route path="signin" element={<SingIn />}></Route>
+        <Route path="signup" element={<SingUp />}></Route>
+        <Route path="profile" element={<Profile />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-// git inti 
-// git add .
-// git comit -m 'install react js and tailwind css and create the first tamplate'
+export default App;
